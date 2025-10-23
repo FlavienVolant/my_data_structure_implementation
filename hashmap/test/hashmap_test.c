e4f71423554b8b97d83b268383c1242fc76db502
Volant <flavienvolant@gmail.com>
2025-10-23 14:23:21 +0200

    use the c_unit lib

--- hashmap/test/hashmap_test.c
+++ hashmap/test/hashmap_test.c
@@ -3,76 +3,19 @@
 #include <assert.h>
 
 #include "hashmap.h"
+#include "c_unit.h"
 
-#define ADD_TEST(list, func) append(list, func, #func)
-
-typedef void(testFunction(struct hashmap *map));
-
-struct List {
-    struct Node *head;
-};
-
-struct Node {
-    testFunction *f;
-    char *name;
-    struct Node *next;
-};
-
-void setup(struct List *list) {
-    list->head = NULL;
+void* beforeEach() {
+    return init_hashmap();
 }
 
-void append(struct List *list, testFunction f, char *name) {
-    struct Node *new = malloc(sizeof(struct Node));
-    new->f = f;
-    new->name = name;
-    new->next = NULL;
-
-    struct Node *tail = list->head;
-
-    if(tail == NULL) {
-        list->head = new;
-        return;
-    }
-
-    while(tail->next != NULL) {
-        tail = tail->next;
-    }
-
-    tail->next = new;
+void afterEach(void* map) {
+    free(map);
 }
 
-void _free(struct Node *node) {
-    if(node == NULL)
-        return;
-    _free(node->next);
-    free(node);
-}
+void put_a_key_value_then_get_the_value_by_giving_the_key(void *params) {
 
-void free_list(struct List *list) {
-    _free(list->head);
-}
-
-void _run_test(testFunction f) {
-    struct hashmap *map = init_hashmap();
-
-    f(map);
-
-    free_hashmap(map);
-}
-
-void run_test(struct List *list) {
-    struct Node *current = list->head;
-
-    while(current != NULL) {
-        printf("Running test: %s\n", current->name);
-        _run_test(current->f);
-        printf("Passed: %s\n\n", current->name);
-        current = current->next;
-    }
-}
-
-void put_a_key_value_then_get_the_value_by_giving_the_key(struct hashmap *map) {
+    struct hashmap *map = params;
 
     int key = 5;
     int value = 23;
@@ -83,7 +26,9 @@ void put_a_key_value_then_get_the_value_by_giving_the_key(struct hashmap *map) {
     assert(res == value);
 }
 
-void put_a_key_bellow_zero_will_work(struct hashmap *map) {
+void put_a_key_bellow_zero_will_work(void *params) {
+
+    struct hashmap *map = params;
 
     int key = - 5;
     int value = 23;
@@ -94,7 +39,9 @@ void put_a_key_bellow_zero_will_work(struct hashmap *map) {
     assert(res == value);
 }
 
-void get_a_key_unknow_will_fail(struct hashmap *map) {
+void get_a_key_unknow_will_fail(void *params) {
+
+    struct hashmap *map = params;
 
     int key = 0;
     int res = -1;
@@ -104,7 +51,9 @@ void get_a_key_unknow_will_fail(struct hashmap *map) {
 
 }
 
-void put_a_key_then_del_give_the_value(struct hashmap *map) {
+void put_a_key_then_del_give_the_value(void *params) {
+
+    struct hashmap *map = params;
 
     int key = 0;
     int value = 0;
@@ -119,17 +68,16 @@ void put_a_key_then_del_give_the_value(struct hashmap *map) {
 
 int main() {
 
-    struct List tests;
-    setup(&tests);
+    struct TestList *tests = create_test_list();
 
-    ADD_TEST(&tests, put_a_key_value_then_get_the_value_by_giving_the_key);
-    ADD_TEST(&tests, put_a_key_bellow_zero_will_work);
-    ADD_TEST(&tests, get_a_key_unknow_will_fail);
-    ADD_TEST(&tests, put_a_key_then_del_give_the_value);
+    ADD_TEST(tests, put_a_key_value_then_get_the_value_by_giving_the_key);
+    ADD_TEST(tests, put_a_key_bellow_zero_will_work);
+    ADD_TEST(tests, get_a_key_unknow_will_fail);
+    ADD_TEST(tests, put_a_key_then_del_give_the_value);
 
-    run_test(&tests);
+    run_tests(tests, beforeEach, afterEach);
 
-    free_list(&tests);
+    free_test_list(tests);
 
     return 0;
 }
